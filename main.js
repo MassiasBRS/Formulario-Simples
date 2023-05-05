3d0fffe5c498c2ef890658df138ebee798b4a449
MassiasBRS <128740905+MassiasBRS@users.noreply.github.com>
2023-05-05 16:00:50 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let login = document.getElementById('botao_submit');
+let cadastro = document.getElementById('botao_cadastro');
+
+function entrar(){
+    alert('Login realizado com sucesso');
+}
+function registrar(){
+    alert('Cadastrado com Sucesso! Seja bem vindo!');
+}
